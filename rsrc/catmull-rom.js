ef854ad5d95d44b899954a5e20b9c93a61162560
Pedro <pedro.hfn7@gmail.com>
2014-06-17 17:27:59 -0300

    AEAEAEAEHAIUEHAIUEHAIUHEYGFOUWYEGBVACLKJSAHDBV

--- rsrc/catmull-rom.js
+++ rsrc/catmull-rom.js
@@ -4,6 +4,7 @@ function createCatmullRom() {
 		controlPoints: [],
 		segments: [],
 		numberSegments: 0,
+		a: 0.5,
 		setControlPoints: function(pts){
 			if(pts.length >= 4){
 				this.controlPoints = pts;
@@ -15,7 +16,49 @@ function createCatmullRom() {
 				this.numberSegments = 0;
 			}
 		},
-		buildForSegment: function(segmentNumber){
+		secondBezier: function(p0, p1, p2){
+			var self = this;
+
+			var v0 = new THREE.Vector3(p0.x, p0.y, p0.z);
+			var v1 = new THREE.Vector3(p1.x, p1.y, p1.z);
+			var v2 = new THREE.Vector3(p2.x, p2.y, p2.z);
+
+			var d1 = Math.abs(v0.distanceTo(v1));
+			var d2 = Math.abs(v1.distanceTo(v2));
+
+			var d1a = Math.pow(d1, self.a);
+			var d2a = Math.pow(d2, self.a);
+
+			var d1aa = Math.pow(d1, (2*self.a));
+			var d2aa = Math.pow(d2, (2*self.a));
+
+			return {
+				x: ((d1aa*p2.x) - (d2aa*p0.x) + (((2*d1aa) + (3*d1a*d2a) + d2aa)*p1.x)) / ((3*d1a) * (d1a + d2a)),
+				y: ((d1aa*p2.y) - (d2aa*p0.y) + (((2*d1aa) + (3*d1a*d2a) + d2aa)*p1.y)) / ((3*d1a) * (d1a + d2a))
+			}
+		},
+		thirdBezier: function(p1, p2, p3){
+			var self = this;
+
+			var v1 = new THREE.Vector3(p1.x, p1.y, p1.z);
+			var v2 = new THREE.Vector3(p2.x, p2.y, p2.z);
+			var v3 = new THREE.Vector3(p3.x, p3.y, p3.z);
+
+			var d2 = Math.abs(v1.distanceTo(v2));
+			var d3 = Math.abs(v2.distanceTo(v3));
+
+			var d2a = Math.pow(d2, self.a);
+			var d3a = Math.pow(d3, self.a);
+
+			var d2aa = Math.pow(d2, (2*self.a));
+			var d3aa = Math.pow(d3, (2*self.a));
+
+			return {
+				x: ((d3aa*p1.x) - (d2aa*p3.x) + (((2*d3aa) + (3*d3a*d2a) + d2aa)*p2.x)) / ((3*d3a) * (d3a + d2a)),
+				y: ((d3aa*p1.y) - (d2aa*p3.y) + (((2*d3aa) + (3*d3a*d2a) + d2aa)*p2.y)) / ((3*d3a) * (d3a + d2a))
+			}
+		},
+		buildForSegmentPolinomial: function(segmentNumber){
 			var self = this;
 			
 			var p0 = self.controlPoints[segmentNumber]
@@ -38,6 +81,27 @@ function createCatmullRom() {
 						( ((-1*p0.y) + (3*p1.y) - (3*p2.y) + p3.y) * (t*(t*t)) ) 
 					))
 				}
+			};
+		},
+		buildForSegment: function(segmentNumber){
+			var self = this;
+			
+			var p0 = self.controlPoints[segmentNumber]
+			var p1 = self.controlPoints[segmentNumber + 1];
+			var p2 = self.controlPoints[segmentNumber + 2];
+			var p3 = self.controlPoints[segmentNumber + 3];
+
+			var b0 = p1;
+			var b1 = self.secondBezier(p0, p1, p2);
+			var b2 = self.thirdBezier(p1, p2, p3);
+			var b3 = p2;
+
+			var bezier = createBezier();
+			bezier.setControlPoints([b0, b1, b2, b3]);
+			var fn = bezier.nivel[bezier.numberLevels][0];
+
+			return function(t){
+				return fn(t);
 			};
 		},
 		buildSegments: function(){
